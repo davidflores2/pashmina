b135e18c4fe3c8c7ae5de683a0fce112de2136bf
ANABETSY RIVERO <anabetsy001@gmail.com>
2016-04-16 22:49:59 -0400

    Added Products

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('layout')
+
+@section('header')
+    <div class="page-header">
+        <h1><i class="glyphicon glyphicon-edit"></i> Products / Edit #{{$product->id}}</h1>
+    </div>
+@endsection
+
+@section('content')
+    @include('error')
+
+    <div class="row">
+        <div class="col-md-12">
+
+            <form action="{{ route('products.update', $product->id) }}" method="POST">
+                <input type="hidden" name="_method" value="PUT">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group @if($errors->has('name')) has-error @endif">
+                       <label for="name-field">Name</label>
+                    <input type="text" id="name-field" name="name" class="form-control" value="{{ $product->name }}"/>
+                       @if($errors->has("name"))
+                        <span class="help-block">{{ $errors->first("name") }}</span>
+                       @endif
+                    </div>
+                    <div class="form-group @if($errors->has('description')) has-error @endif">
+                       <label for="description-field">Description</label>
+                    <textarea class="form-control" id="description-field" rows="3" name="description">{{ $product->description }}</textarea>
+                       @if($errors->has("description"))
+                        <span class="help-block">{{ $errors->first("description") }}</span>
+                       @endif
+                    </div>
+                    <div class="form-group @if($errors->has('price')) has-error @endif">
+                       <label for="price-field">Price</label>
+                    <input type="text" id="price-field" name="price" class="form-control" value="{{ $product->price }}"/>
+                       @if($errors->has("price"))
+                        <span class="help-block">{{ $errors->first("price") }}</span>
+                       @endif
+                    </div>
+                    <div class="form-group @if($errors->has('countryoforigin')) has-error @endif">
+                       <label for="countryoforigin-field">CountryOfOrigin</label>
+                    <input type="text" id="countryoforigin-field" name="countryoforigin" class="form-control" value="{{ $product->countryoforigin }}"/>
+                       @if($errors->has("countryoforigin"))
+                        <span class="help-block">{{ $errors->first("countryoforigin") }}</span>
+                       @endif
+                    </div>
+                <div class="well well-sm">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                    <a class="btn btn-link pull-right" href="{{ route('products.index') }}"><i class="glyphicon glyphicon-backward"></i>  Back</a>
+                </div>
+            </form>
+
+        </div>
+    </div>
+@endsection
