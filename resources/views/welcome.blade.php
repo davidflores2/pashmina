94c9464e783e8ebd257d27ec677d0e72f3717074
David Flores <david@eatable.at>
2016-04-17 10:41:54 -0400

    styling changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,19 +9,19 @@
             <p>Share. Buy. Support</p>
             <a class="btn btn-primary cta" href="{{ url('/products/1') }}">Explore</a>
         </div>
-        <div class="content row">
-            <div class="col-xs-6 col-sm-4 a">
+        <div class="content">
+            <div class="col-md-4 a">
                 <img src= "/assets/imgs/mic.png" alt="Weave">        
                 <h2>Share</h2>
                 <p>Share your country's charity projects and "product of the day" to encourage 
 people to cooperate! That countries' full market will be unlocked for that person. Build your map!</p>
             </div>
-            <div class="col-xs-6 col-sm-4 a">
+            <div class="col-md-4 a">
                 <img src= "/assets/imgs/cart.png" alt="Weave">        
                 <h2>Buy</h2>
                 <p>Your purchase will support artisans from all over the world while contributing to social causes in the country of your heritage or the markets you unlock through sharing.</p>
             </div>
-            <div class="col-xs-6 col-sm-4 a">
+            <div class="col-md-4 a">
                 <img src= "/assets/imgs/people.png" alt="Weave">        
                 <h2>Support</h2>
                 <p>Help others through value the work they already do, and make their
